bc08927216b7ecd324c598b9e7c41613f6634e2a
Patilshrikrushna07 <93507840+Patilshrikrushna07@users.noreply.github.com>
2023-10-31 18:05:28 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import Hero from "./Parallax";
+import Scroll from "./Scroll";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Hero /> */}
+      <Scroll />
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
